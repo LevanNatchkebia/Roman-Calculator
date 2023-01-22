013f237416795cf5c284d3cec5e42d889b8749e3
Levan Natchkebia <116100909+LevanNatchkebia@users.noreply.github.com>
2023-01-23 02:09:10 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <string>
 using namespace std;
 
-map<char, int> romanMap;                                    
+//############################################################################
+//maping roman numerals to integer 
+map<char, int> romanMap;                                   
 
 void symbolMap() {
     romanMap['I'] = 1;
@@ -10,10 +14,11 @@ void symbolMap() {
     romanMap['X'] = 10;
     romanMap['L'] = 50;
     romanMap['C'] = 100;
-    romanMap['D'] = 500;
-    romanMap['M'] = 1000;
 }
 
+
+//############################################################################
+//convert romanian symbols to integer
 int romanToInt(string roman) {
     int result = 0;
     for (int i = 0; i < roman.length(); i++) {
@@ -26,6 +31,9 @@ int romanToInt(string roman) {
     return result;
 }
 
+
+//############################################################################
+//convert integers to romanian symbols
 string intToRoman(int num) {
     string roman[] = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     int values[] = {100, 90, 50, 40, 10, 9, 5, 4, 1};
@@ -39,25 +47,117 @@ string intToRoman(int num) {
     return result;
 }
 
+
+
+
 int main() {
+    
     symbolMap();
-    string input, num1, operatorSymbol;
+    
+    
+    string input, num1, operatorSymbol, number;
     int  result;
+    
+    
+    
     cout<<"Welcome, I am Roman Numeral Calculator"<<endl;
     cout<< endl;
     cout<<"Please Enter Roman Numeral: ";
     cin>>input;
     
+    
+    
     result = romanToInt(input);
     
+    number = to_string(result);
+    
+    
+    
+//############################################################################
+// create vectors for romanian symbols and for numbers, for printing result and complete arithmetical actions  
+    vector <string> symb;
+    vector <string> nums;
+    
+    
+    
+    
+    symb.push_back(input);
+    nums.push_back(number);
+    
+    
+    
+    
     while (true) {
-        cout<<"Select from following operators: +, -, *, /"<<endl;
-        cout<<"Please Enter operator: ";
-        cin>>operatorSymbol;
+  //############################################################################
+  // checks if 'exit' is entered in number entering level and remove word 'exit' and arithmetical symbol from vector and adds it '=' and result.
+        if (num1=="exit") {
+            symb.pop_back();
+            symb.pop_back();
+            symb.push_back("=");
+            symb.push_back(intToRoman(result));
+            for (int i=0; i< symb.size(); i++){
+                cout<< symb[i];
+             
+            } 
+            
+            cout<< endl;
+            
+            
+            nums.pop_back();
+            nums.pop_back();
+            nums.push_back("=");
+            nums.push_back(to_string(result));
+            for (int i=0; i< nums.size(); i++){
+                cout<< nums[i];
+             
+            }
+            
+            break;
+        }
+        
+        
+        
+   //############################################################################
+  // checks if 'exit' is entered in arithmetical symbol entering level and remove word 'exit' from vector and adds it '=' and result.     
+        
+        if (operatorSymbol=="exit") {
+            symb.pop_back();
+            symb.push_back("=");
+            symb.push_back(intToRoman(result));
+            for (int i=0; i< symb.size(); i++){
+                cout<< symb[i];
+             
+            }
+            
+            cout<< endl;
+            nums.pop_back();
+            nums.push_back("=");
+            nums.push_back(to_string(result));
+            for (int i=0; i< nums.size(); i++){
+                cout<< nums[i];
+              
+            }
+            break;
+        }
+        
+ //########################################################################################
+ //checks for how long loop must be used and what operations must be done
+        if (num1!= "exit") {
+            cout<<"Select from following operators: +, -, *, /"<<endl;
+            cout<<"Please Enter operator: ";
+            cin>>operatorSymbol;
+            symb.push_back(operatorSymbol);
+            nums.push_back(operatorSymbol);
+        }
+        
+        
         
         if (operatorSymbol != "exit") {
             cout<<"Enter next Roman Numeral: ";
             cin>>num1;
+            symb.push_back(num1);
+            nums.push_back(to_string(romanToInt(num1)));
+            
         }
         if (operatorSymbol == "+") {
             result = result + romanToInt(num1);
@@ -71,17 +171,10 @@ int main() {
         else if (operatorSymbol == "/") {
             result = result / romanToInt (num1);
         }
-        else if (operatorSymbol=="exit" || num1 == "exit") {
-            cout<< intToRoman(result)<<endl;
-            break;
-        }
+        
+    
+        
         
     }
-    
-    
     return 0;
 }
-
-
-
-
